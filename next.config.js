b5a681893d56ec3d38c7260d5b0996ca28bfbb64
Giordan Masen <giordan.masen@gmail.com>
2026-06-04 16:04:01 +0930

    im down

--- next.config.js
+++ next.config.js
@@ -1,33 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  redirects: async () => [
-    // Redirect HTTP to HTTPS
-    {
-      source: '/:path*',
-      destination: 'https://untitledtrading.com/:path*',
-      permanent: true,
-      has: [
-        {
-          type: 'header',
-          key: 'x-forwarded-proto',
-          value: 'http',
-        },
-      ],
-    },
-    // Redirect www to non-www
-    {
-      source: '/:path*',
-      destination: 'https://untitledtrading.com/:path*',
-      permanent: true,
-      has: [
-        {
-          type: 'host',
-          value: 'www.untitledtrading.com',
-        },
-      ],
-    },
-  ],
 };
 
 module.exports = nextConfig;
